bce6c67b5059e7025adecbdcdfb68ff35362322c
Tanjiro_1709 <147371895+TANJIRO1709@users.noreply.github.com>
2024-11-30 02:29:29 +0530

    Delete Downloads/Project-Coal-main directory

--- Downloads/Project-Coal-main/server/routes/combinedLeaderboardRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getLeaderboard } = require('../controllers/leaderboardController');
-router.get('/combinedleaderboard', getLeaderboard);
-module.exports = router;
